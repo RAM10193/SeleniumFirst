cd82bb90caba755f3c429272f74f62e392e35a82
RAM10193 <gkmram@gmail.com>
2022-06-04 00:27:04 +0530

    Add project files.

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.Events;
+
+namespace SeleniumFirst
+{
+    
+    class Program
+    {
+        IWebDriver dr = new ChromeDriver();
+        SeleniumSetMethods setMethods = null;
+        String agreebutton = "//button[contains(. ,'I agree')]";
+        String textBox = "//span[contains(.,'Text Box')]";
+       
+
+        static void Main(String[] args)
+        {
+
+        }
+
+        [SetUp]
+        public void initialize()
+        {
+            dr.Navigate().GoToUrl("https://demoqa.com/elements");
+            dr.Manage().Window.Maximize();
+            if (dr.FindElements(By.XPath("//button[contains(. ,'I agree')]")).Count() > 0)
+                setMethods.clickElement("xpath", agreebutton);
+           
+            Console.WriteLine("Browser launched");
+        }
+        
+        [Test]
+        public void action()
+        {
+            WebDriverWait wait = new WebDriverWait(dr, TimeSpan.FromSeconds(10));
+            if (dr.FindElements(By.XPath(textBox)).Count > 0)
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(textBox)));
+                setMethods.clickElement("xpath", textBox);
+            }
+            else
+                throw new NoSuchElementException();
+            Console.WriteLine("Elements card clicked");
+        }
+
+        [TearDown]
+        public void cleanup()
+        {
+            dr.Quit();
+            Console.WriteLine("Browser closed");
+        }
+    }
+}
---
+++ SeleniumFirst/SeleniumSetMethods.cs
@@ -0,0 +1,63 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    public class SeleniumSetMethods
+    {
+        public IWebDriver dr;
+        public void seleniumSetMethods(IWebDriver driver)
+        {
+            this.dr = driver; 
+              
+        }
+        public void typeText(String elementType, String locator, String value)
+        {
+            if (elementType.ToLower() == "id")
+                dr.FindElement(By.Id(locator)).SendKeys(value);
+            else if (elementType.ToLower() == "name")
+                dr.FindElement(By.Id(locator)).SendKeys(value);
+            else if (elementType.ToLower() == "Xpath")
+                dr.FindElement(By.Id(locator)).SendKeys(value);
+            else
+                throw new ElementNotVisibleException();
+        }
+
+        public void clickElement(String elementType, String locator)
+        {
+            try {
+            if (elementType.ToLower() == "id")
+                dr.FindElement(By.Id(locator)).Click();
+            else if (elementType.ToLower() == "name")
+                dr.FindElement(By.Id(locator)).Click();
+            else if (elementType.ToLower() == "xpath")
+                dr.FindElement(By.Id(locator)).Click();
+            }
+            catch 
+            {
+                throw new ElementClickInterceptedException();
+            }
+             
+        }
+
+
+        public void SelectvaluesDropdown(String elementType, String locator, String value)
+        {
+
+             if (elementType == "id")
+                new SelectElement(dr.FindElement(By.Id(locator))).SelectByValue(value);
+            else if (elementType == "name")
+                new SelectElement(dr.FindElement(By.Id(locator))).SelectByValue(value);
+            else if (elementType == "Xpath")
+                new SelectElement(dr.FindElement(By.Id(locator))).SelectByValue(value);
+            else
+                throw new ElementClickInterceptedException();
+        }
+
+    }
+}
