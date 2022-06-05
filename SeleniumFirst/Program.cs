9673fbdb3211b4dd8b8b787812f932092e9b22c5
RAM10193 <gkmram@gmail.com>
2022-06-06 02:47:39 +0530

    TestcaseUpdate

--- SeleniumFirst/Program.cs
+++ SeleniumFirst/Program.cs
@@ -15,10 +15,15 @@ namespace SeleniumFirst
     class Program
     {
         IWebDriver dr = new ChromeDriver();
-        SeleniumSetMethods setMethods = null;
         String agreebutton = "//button[contains(. ,'I agree')]";
         String textBox = "//span[contains(.,'Text Box')]";
-       
+        String username = "//input[@id='userName']";
+        String userEmail = "//input[@id='userEmail']";
+        String usercurrAddress = "//textarea[@id='currentAddress']";
+        String userPermAddress = "//textarea[@id='permanentAddress']";
+        String btnSubmit = "//button[@id='submit']";
+        String txtcurrAdd = "//p[@id='currentAddress']";
+        
 
         static void Main(String[] args)
         {
@@ -26,28 +31,67 @@ namespace SeleniumFirst
         }
 
         [SetUp]
-        public void initialize()
+        public void onlineFormLaunch()
         {
             dr.Navigate().GoToUrl("https://demoqa.com/elements");
             dr.Manage().Window.Maximize();
             if (dr.FindElements(By.XPath("//button[contains(. ,'I agree')]")).Count() > 0)
-                setMethods.clickElement("xpath", agreebutton);
-           
-            Console.WriteLine("Browser launched");
+            {
+                SeleniumSetMethods.clickElement(dr,"xpath", agreebutton);
+            }
+                          
+            Console.WriteLine("Browser launched");            
+
         }
         
-        [Test]
-        public void action()
+        [Test]       
+        public void onlineFormSubmission()
         {
-            WebDriverWait wait = new WebDriverWait(dr, TimeSpan.FromSeconds(10));
-            if (dr.FindElements(By.XPath(textBox)).Count > 0)
+            
+            if (dr.FindElement(By.XPath(textBox)).Displayed)
             {
-                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(textBox)));
-                setMethods.clickElement("xpath", textBox);
+                SeleniumSetMethods.clickElement(dr,"xpath", textBox);
+                SeleniumSetMethods.typeText(dr, "xpath", username, "Ramkumar Krishnamurthy");
+                SeleniumSetMethods.typeText(dr, "xpath", userEmail, "gkmram@gmail.com");
+                SeleniumSetMethods.typeText(dr, "xpath", usercurrAddress , "123 Dowling Street");
+                SeleniumSetMethods.typeText(dr, "xpath", userPermAddress, "123 Dowling Street");
+                if (dr.FindElement(By.XPath(btnSubmit)).Displayed)
+                {
+                    SeleniumSetMethods.ActionsclickElement(dr, "xpath", btnSubmit);
+                }
+                    
+                outputValidation();
+                Console.WriteLine("Form filled and submitted successfully");
             }
             else
+            {
                 throw new NoSuchElementException();
-            Console.WriteLine("Elements card clicked");
+            }
+
+        }
+
+        
+        public void outputValidation()
+        {
+
+            if (dr.FindElement(By.XPath(btnSubmit)).Displayed)
+            {
+                String fulladdress = SeleniumGetMethods.GetText(dr,"xpath", txtcurrAdd);
+                String[] address = fulladdress.Split(':');
+                if(SeleniumGetMethods.TextValidation(dr,"123 Dowling Street",address[1]))
+                {
+                    Console.WriteLine("Form filled and submitted successfully");
+                }
+                else
+                {
+                    throw new AssertionException("current address not valid and hence failed.");
+                }
+            }
+            else
+            {
+                throw new NoSuchElementException();
+            }
+
         }
 
         [TearDown]
--- SeleniumFirst/SeleniumSetMethods.cs
+++ SeleniumFirst/SeleniumSetMethods.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -10,51 +11,77 @@ namespace SeleniumFirst
 {
     public class SeleniumSetMethods
     {
-        public IWebDriver dr;
-        public void seleniumSetMethods(IWebDriver driver)
-        {
-            this.dr = driver; 
-              
-        }
-        public void typeText(String elementType, String locator, String value)
+        
+        public static void typeText(IWebDriver dr , String elementType, String locator, String value)
         {
             if (elementType.ToLower() == "id")
                 dr.FindElement(By.Id(locator)).SendKeys(value);
             else if (elementType.ToLower() == "name")
-                dr.FindElement(By.Id(locator)).SendKeys(value);
-            else if (elementType.ToLower() == "Xpath")
-                dr.FindElement(By.Id(locator)).SendKeys(value);
+                dr.FindElement(By.Name(locator)).SendKeys(value);
+            else if (elementType.ToLower() == "xpath")
+                dr.FindElement(By.XPath(locator)).SendKeys(value);
             else
                 throw new ElementNotVisibleException();
         }
 
-        public void clickElement(String elementType, String locator)
+        public static void clickElement(IWebDriver dr , String elementType, String locator)
         {
-            try {
             if (elementType.ToLower() == "id")
                 dr.FindElement(By.Id(locator)).Click();
             else if (elementType.ToLower() == "name")
-                dr.FindElement(By.Id(locator)).Click();
+                dr.FindElement(By.Name(locator)).Click();
             else if (elementType.ToLower() == "xpath")
-                dr.FindElement(By.Id(locator)).Click();
-            }
-            catch 
-            {
+                dr.FindElement(By.XPath(locator)).Click();
+            else
                 throw new ElementClickInterceptedException();
-            }
+            
              
         }
 
+        public static void ActionsclickElement(IWebDriver dr, String elementType, String locator)
+        {
+            Actions actions = new Actions(dr);
+            IJavaScriptExecutor ex = (IJavaScriptExecutor)dr;
+            if (elementType.ToLower() == "id")
+                  ex.ExecuteScript("arguments[0].click();", dr.FindElement(By.Id(locator)));
+            else if (elementType.ToLower() == "name")
+                  ex.ExecuteScript("arguments[0].click();", dr.FindElement(By.Name(locator)));
+            else if (elementType.ToLower() == "xpath")          
+                  ex.ExecuteScript("arguments[0].click();", dr.FindElement(By.XPath(locator)));
+            else
+                throw new ElementClickInterceptedException();
 
-        public void SelectvaluesDropdown(String elementType, String locator, String value)
+
+        }
+
+        /*public static void SubmitForm(IWebDriver dr, String elementType, String locator)
+        {
+            try
+            {
+                if (elementType.ToLower() == "id")
+                    dr.FindElement(By.Id(locator)).Submit();
+                else if (elementType.ToLower() == "name")
+                    dr.FindElement(By.Name(locator)).Submit();
+                else if (elementType.ToLower() == "xpath")
+                    dr.FindElement(By.XPath(locator)).Submit();
+            }
+            catch
+            {
+                throw new ElementClickInterceptedException();
+            }
+
+        }*/
+
+
+        public static void SelectvaluesDropdown(IWebDriver dr , String elementType, String locator, String value)
         {
 
-             if (elementType == "id")
+             if (elementType.ToLower() == "id")
                 new SelectElement(dr.FindElement(By.Id(locator))).SelectByValue(value);
-            else if (elementType == "name")
-                new SelectElement(dr.FindElement(By.Id(locator))).SelectByValue(value);
-            else if (elementType == "Xpath")
-                new SelectElement(dr.FindElement(By.Id(locator))).SelectByValue(value);
+            else if (elementType.ToLower() == "name")
+                new SelectElement(dr.FindElement(By.Name(locator))).SelectByValue(value);
+            else if (elementType.ToLower() == "xpath")
+                new SelectElement(dr.FindElement(By.XPath(locator))).SelectByValue(value);
             else
                 throw new ElementClickInterceptedException();
         }
